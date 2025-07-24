a015f82431c2aae5c8e71dc07817e43b618de2e5
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-24 16:56:43 +0100

    new commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-md fixed w-full z-1 top-0 left-0">
+        <div className='max-w-7xl mx-auto px-6 py-4 flex justify-between items-center'>
+           <a href="#" className='font-semibold hover:text-blue-600 '></a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
