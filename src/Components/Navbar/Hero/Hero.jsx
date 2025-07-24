27dd1d91a467e9cc8f95038eafbe497306802d85
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-24 18:02:22 +0100

    Added bg

---
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Hero = () => {
+  return (
+    <section className="relative bg-cover bg-center h-screen bg-[url('./assets/RealEstate.png')] flex items-center justify-center text-center bg-blue-500"></section>
+  )
+}
+
+export default Hero
