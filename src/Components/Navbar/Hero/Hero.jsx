963fa660a00fb64b053a2861069067cd3d1fd206
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-24 18:29:51 +0100

    new commit

--- src/Components/Navbar/Hero/Hero.jsx
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <section className="relative bg-cover bg-center h-screen bg-[url('./assets/RealEstate.png')] flex items-center justify-center text-center bg-blue-500">
-        
+
           <div className="relative z-10 text-black p-10 bg-white rounded-t-4xl ">
             <h2 className="text-2xl sm:text-4xl md:text-5xl font-light mb-4 font-serif">Your Best <br />Real Estate Agent</h2>
             <p className="md-6">Comfort, peaceful, Shortlet, Staycation, etc all in best Agent</p>
@@ -13,6 +13,7 @@ const Hero = () => {
             </button>
             <hr className="mt-8 w-30 m-auto border-t-4 border-red-600" />
         </div>
+        
     </section>
   )
 }
