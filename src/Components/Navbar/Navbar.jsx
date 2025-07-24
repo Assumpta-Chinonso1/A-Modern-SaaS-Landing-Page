ce8ccfaa404545e9447c1aa0ecfda1760eca7eea
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-24 16:57:14 +0100

    New commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white shadow-md fixed w-full z-1 top-0 left-0">
         <div className='max-w-7xl mx-auto px-6 py-4 flex justify-between items-center'>
-           <a href="#" className='font-semibold hover:text-blue-600 '></a>
+           <a href="#" className='font-semibold hover:text-blue-600 font-mono'></a>
         </div>
     </nav>
   )
