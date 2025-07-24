0633825a0bc185afec8bcd2b2b7cef031bee5f8e
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-24 18:04:06 +0100

    new commit

--- src/Components/Navbar/Hero/Hero.jsx
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className="relative bg-cover bg-center h-screen bg-[url('./assets/RealEstate.png')] flex items-center justify-center text-center bg-blue-500"></section>
+    <section className="relative bg-cover bg-center h-screen bg-[url('./assets/RealEstate.png')] flex items-center justify-center text-center bg-blue-500">
+        <div className="relative "></div>
+    </section>
   )
 }
 
