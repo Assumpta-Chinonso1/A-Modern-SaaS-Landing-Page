809e8b2c0a256f2009fb3208d99e4225b3c4192e
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-24 17:10:50 +0100

    New commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import {Font}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUmbrella } from '@fortawesome/free-solid-svg-icons'
+
 
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md fixed w-full z-1 top-0 left-0">
         <div className='max-w-7xl mx-auto px-6 py-4 flex justify-between items-center'>
            <a href="#" className='font-semibold hover:text-blue-600 font-mono'>
-               
+                 <FontAwesomeIcon icon={faUmbrella} className="text-purple-600 text-3xl" />
            </a>
         </div>
     </nav>
