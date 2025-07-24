fdfeeffce92de5b280d406f854ae7de004e3a1d4
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-24 18:07:07 +0100

    New commit

--- src/Components/Navbar/Hero/Hero.jsx
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -4,7 +4,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <section className="relative bg-cover bg-center h-screen bg-[url('./assets/RealEstate.png')] flex items-center justify-center text-center bg-blue-500">
-        <div className="relative z-10 p-10 bg-white rounded-t-4xl "></div>
+        <div className="relative z-10 text-black p-10 bg-white rounded-t-4xl ">
+            <h2 className="text-2xl sm:text-4xl md:text-5xl font-light"></h2>
+        </div>
     </section>
   )
 }
