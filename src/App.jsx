082621d110aa21ef309f1b7365d0eb5c67fecdcf
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-25 18:21:26 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,3 +26,41 @@ const App = () => {
 }
 
 export default App*/
+
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Navbar/Hero/Hero'
+import Houses from './Components/Navbar/OurHouses/Houses'
+import Testimonials from './Components/Navbar/Testimonies/Testimonials'
+import Newsletter from './Components/Navbar/NewsLetters/Newsletter'
+import Footer from './Components/Navbar/Footer/Footer'
+import ContactForm from './Components/Navbar/Contact/ContactForm'
+import FaqSection from './Components/Navbar/Questions/FaqSection'
+
+const Home = () => (
+  <>
+    <Hero />
+    <Houses />
+    <Testimonials />
+    <Newsletter />
+  </>
+)
+
+const App = () => {
+  return (
+    <Router>
+      <div className='text-sm md:text-lg'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="/faqs" element={<FaqSection />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
@@ -23,4 +23,28 @@ const Navbar = () => {
   )
 }
 
+export default Navbar */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-md fixed w-full z-10 top-0 left-0">
+      <div className='max-w-7xl mx-auto px-6 py-4 flex justify-between items-center'>
+        <Link to="/" className='font-semibold hover:text-blue-600 font-mono flex items-center gap-2'>
+          <FontAwesomeIcon icon={faHouse} className="text-blue-400 text-3xl" />
+          Real Estate
+        </Link>
+        <ul className='flex gap-4 text-gray-500'>
+          <li><Link to="/" className='hover:text-blue-500'>Home</Link></li>
+          <li><Link to="/contact" className='hover:text-blue-500'>Contact</Link></li>
+          <li><Link to="/faqs" className='hover:text-blue-500'>FAQs</Link></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
 export default Navbar
+
