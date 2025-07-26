65e541eb43e49b91277f7a8a1de877a1db2574e1
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-26 17:49:56 +0100

    new commit

--- src/Components/Navbar/Testimonies/Testimonials.jsx
+++ src/Components/Navbar/Testimonies/Testimonials.jsx
@@ -31,8 +31,9 @@ const Testimonials = () => {
       <div className="text-center mb-10 px-4">
 
         <h2 className="text-3xl font-bold">What Our Clients Say </h2>
-        
+
         <p className="text-gray-600 mt-2">Real stories from satisfied homeowners</p>
+        
       </div>
 
        <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-6">
