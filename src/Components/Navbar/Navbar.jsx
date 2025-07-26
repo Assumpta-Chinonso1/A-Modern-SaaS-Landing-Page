e14dcff3d2038ef8ec545cd5b6b483e0ffa6f52f
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-26 17:57:09 +0100

    new commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
           className={`fixed top-0 left-0 w-full h-full bg-white flex flex-col items-center justify-center gap-8 text-xl text-gray-700 transition-transform duration-300 ease-in-out ${
             menuOpen ? 'translate-x-0' : '-translate-x-full'
           }`} >
-            
+
           {['home', 'houses', 'contact', 'faqs'].map((item) => (
             <ScrollLink
               key={item}
@@ -62,8 +62,7 @@ const Navbar = () => {
               duration={500}
               offset={-70}
               className="cursor-pointer hover:text-blue-500 capitalize"
-              onClick={closeMenu}
-            >
+              onClick={closeMenu}     >
               {item}
             </ScrollLink>
           ))}
