2086190ff5ce7a9fcc3fa407dae0aed9833df551
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-26 17:53:23 +0100

    new commit

--- src/Components/Navbar/Testimonies/Testimonials.jsx
+++ src/Components/Navbar/Testimonies/Testimonials.jsx
@@ -31,6 +31,7 @@ const Testimonials = () => {
 
   return (
     <section className="bg-gray-100 py-16">
+      
       <div className="text-center mb-10 px-4">
 
         <h2 className="text-3xl font-bold">What Our Clients Say </h2>
