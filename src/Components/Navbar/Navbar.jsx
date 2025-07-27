e3f132b9f93c73209bc383adca1fdc7ed792ede5
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-27 20:31:09 +0100

    new commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
         {/* Fullscreen Mobile Menu */}
         <div
           className={`fixed top-0 left-0 w-full h-full bg-white flex flex-col items-center justify-center gap-8 text-xl text-gray-700 transition-transform duration-300 ease-in-out ${
-            menuOpen ? 'translate-x-0' : '-translate-x-full'
+             menuOpen ? 'translate-x-0' : '-translate-x-full'
           }`}>
 
           {['home', 'houses', 'contact', 'faqs'].map((item) => (
