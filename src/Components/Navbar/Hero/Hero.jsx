410bbd558ca656f6622418637f9e01f1f3bbde08
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-30 15:46:24 +0100

    new commit

--- src/Components/Navbar/Hero/Hero.jsx
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
              <div className="relative z-10 text-black p-10 bg-white rounded-t-4xl ">
                 <h2 className="text-2xl sm:text-4xl md:text-5xl font-light mb-4 font-serif">Your Best <br />Real Estate Agent</h2>
                 <p className="md-6">Comfort, peaceful, Shortlet, Staycation, etc all in best Agent</p>
-            <br />
+                   <br />
             <button className="cursor-pointer text-black bg-blue-400 px-6 py-4 rounded-full font-semibold hover:bg-blue-500">
             Build your Dream Home
             </button>
